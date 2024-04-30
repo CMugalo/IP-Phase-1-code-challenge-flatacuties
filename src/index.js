d3848ddd402c524d4cb81de9af8f0342efcf9b86
Christian Mugalo <christian.mugalo@student.moringaschool.com>
2024-04-30 08:01:08 +0300

    updated submit event

--- src/index.js
+++ src/index.js
@@ -1,62 +1,48 @@
 // Your code here
+
 document.addEventListener("DOMContentLoaded", () => {
-    // Fetch request for the character resources
-  
-    function getAllCharacters() {
-      fetch("http://localhost:3000/characters")
-        .then((res) => res.json())
-        .then((characters) =>
-          characters.forEach((character) => renderOneCharacter(character))
-        );
-    }
-  
-    /////////////////////////////////////////////////
-  
-    // Building elements and DOM Render Functions
-  
-    function renderOneCharacter(character) {
-      // Add character names to DOM
-      const card = document.createElement("span");
-      card.textContent = character.name;
-      document.querySelector("#character-bar").appendChild(card);
-  
-      /////////////////////////////////////////////////
-  
-      // Adding click event listener
-      card.addEventListener("click", (e) => {
-        e.preventDefault;
-        const gifOnDom = document.querySelector("#image");
-        gifOnDom.src = character.image;
-        const gifName = document.querySelector("#name");
-        gifName.textContent = character.name;
-        const gifVotes = document.querySelector("#vote-count");
-        gifVotes.textContent = character.votes;
-      });
-    }
-  
-    /////////////////////////////////////////////////
-  
-    // Adding submit event listener & displaying votes keyed in (No persistence)
-  
-    const votingForm = document.querySelector("#votes-form");
-    votingForm.addEventListener("submit", (e) => {
-      e.preventDefault();
-      totalVotes(e.target.votes.value);
-      votingForm.reset();
+  const characterBar = document.getElementById("character-bar");
+  const detailedInfo = document.getElementById("detailed-info");
+  const gifName = document.getElementById("name");
+  const gifOnDom = document.getElementById("image");
+  const gifVotes = document.getElementById("vote-count");
+  const votingForm = document.getElementById("votes-form");
+  let currentCharacter;
+
+  // Fetch request for the characters
+
+  fetch("http://localhost:3000/characters")
+    .then((res) => res.json())
+    .then(getAllCharacters);
+
+  function getAllCharacters(characters) {
+    characters.forEach(renderOneCharacter);
+  }
+
+  // Adding elements to the DOM
+  function renderOneCharacter(character) {
+    const card = document.createElement("span");
+    card.textContent = character.name;
+    characterBar.append(card);
+
+    // Adding click event listener
+    card.addEventListener("click", () => {
+      currentCharacter = character;
+      displayInfo(character);
     });
-  
-    function totalVotes(animalVotes) {
-      if (isNaN(animalVotes)) {
-        return alert("Votes must be a number!");
-      } else {
-        console.log(animalVotes);
-        const displayVotes = document.querySelector("#vote-count");
-        displayVotes.textContent = animalVotes;
-      }
-    }
-  
-    function initialize() {
-      getAllCharacters();
-    }
-    initialize();
+  }
+
+  // Adding submit event listener & displaying votes keyed in
+  votingForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    currentCharacter.votes += parseInt(e.target.votes.value);
+    displayInfo(currentCharacter);
+    votingForm.reset()
   });
+
+  function displayInfo(character) {
+    gifName.textContent = character.name;
+    gifOnDom.src = character.image;
+    gifVotes.textContent = character.votes;
+  }
+});
